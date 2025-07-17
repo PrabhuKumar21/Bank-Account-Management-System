0824d64810a595e6cd0e46162114fa56509df8f5
Prabhu Kumar <peddisettiprabhukumar@gmail.com>
2025-07-17 20:40:31 +0530

    made change in User entity table name as users

--- src/main/java/com/bams/entity/User.java
+++ src/main/java/com/bams/entity/User.java
@@ -1,16 +1,26 @@
 package com.bams.entity;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.*;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Entity
@@ -18,7 +28,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table
+@Table(name="users")
 public class User {
 
 	@Id
